0642065c3cf8e9278dc51ccb056be093d0e280d9
dev2 <mark-toth@outlook.com>
2019-10-21 13:37:57 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { todos: [] };
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    pushItemIntoArray = (newItem) => {      
+        this.setState({ todos: [...this.state.todos, newItem]})                    
+    };
+    render() {
+        return (
+        <div>
+        <MyForm onButtonPress={this.pushItemIntoArray}></MyForm>
+        <List todos={this.state.todos}></List>
+        </div>    
+        )     
+    }
+};
+
+class MyForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {          
+            newValue: ''
+    };
+}
+
+    storeCurrentUserInput = (event) => {
+        let newValue = event.target.value;
+        this.setState({ newValue: newValue })
+    };
+
+    sendValueToApp = (e) => {
+        this.props.onButtonPress(this.state.newValue);
+        this.setState({newValue : ''})
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form>
+                <h1>Hello</h1>
+                <p>Enter your todo:</p>
+                <input
+                    type="text"
+                    onChange={this.storeCurrentUserInput}
+                    value = {this.state.newValue}
+                />
+                <button onClick={this.sendValueToApp}></button>
+            </form>
+
+        )
+    }
+}
+
+class List extends React.Component {
+    render() {
+        let names = this.props.todos;
+        return (
+            <ul>
+                {names.map(function(name, index){
+                    return <li onClick={console.log('it has been clicked')} key={ index }>{name}</li>;
+                  })}
+            </ul>
+        )
+    }
+};
+
+ReactDOM.render(
+    <App>       
+    </App>    
+    ,
+    document.getElementById('root')
+);
